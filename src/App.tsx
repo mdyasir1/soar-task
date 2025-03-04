7d9fa7d11ac54f01accd3f681e50923e7bc77ac1
mdyasir1 <mdyasir4145@gmail.com>
2025-03-04 09:54:17 +0530

    added some packages

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='p-8 bg-red-900'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
