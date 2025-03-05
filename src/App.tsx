921d437ac8b89d2494843eac7d06d96d3571a300
mdyasir1 <mdyasir4145@gmail.com>
2025-03-05 12:33:18 +0530

    checking at 9:41

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/button-has-type */
+/* eslint-disable prettier/prettier */
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+// eslint-disable-next-line import/no-unresolved
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -8,26 +12,22 @@ function App() {
 
   return (
     <>
-      <div className='p-8 bg-red-900'>
-        <a href="https://vite.dev" target="_blank">
+      <div className="p-8 bg-red-900">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   )
 }
