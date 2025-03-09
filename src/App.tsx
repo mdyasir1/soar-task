8ba7ec33844770f0bd9b755ca6342a6aeff5538d
mdyasir1 <mdyasir4145@gmail.com>
2025-03-09 16:19:50 +0530

    chore: card-dynamic-component with color-variants ADDED

--- src/App.tsx
+++ src/App.tsx
@@ -15,35 +15,35 @@ function App() {
         balance="2205"
         expiry="11/35"
         cardNumber="9012 5678 1234"
-        name="Spencer Boone"
+        name="John Doe"
         variant="white"
       />
       <Card
         balance="9025"
         expiry="07/28"
         cardNumber="5678 9012 1234"
-        name="Emerson Cortez"
+        name="John Doe"
         variant="dark"
       />
       <Card
         balance="2125"
         expiry="09/26"
         cardNumber="5678 1234 9012"
-        name="Zariah Gomez"
+        name="John Doe"
         variant="white"
       />
       <Card
         balance="9025"
         expiry="07/28"
         cardNumber="5678 9012 1234"
-        name="Emmalynn Villar"
+        name="John Doe"
         variant="dark"
       />
       <Card
         balance="2125"
         expiry="09/26"
         cardNumber="5678 1234 9012"
-        name="Liliana Johnston"
+        name="John Doe"
         variant="white"
       />
     </div>
