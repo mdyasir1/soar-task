5cc07dd0c5b42490c0750483f7c6d53ab0de5726
mdyasir1 <mdyasir4145@gmail.com>
2025-03-09 15:57:00 +0530

    chore: card-dynamic-component with color-variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
+// import Another from './components/another'
 import Card from './components/Card'
 
 function App() {
   return (
-    <div>
-      <Card />
+    <div id="card-holder" className="flex content-center">
+      <Card
+        balance="5225"
+        expiry="12/34"
+        cardNumber="1234 5678 9012"
+        name="John Doe"
+        variant="dark"
+      />
+      <Card
+        balance="2205"
+        expiry="11/35"
+        cardNumber="9012 5678 1234"
+        name="Spencer Boone"
+        variant="white"
+      />
+      <Card
+        balance="9025"
+        expiry="07/28"
+        cardNumber="5678 9012 1234"
+        name="Emerson Cortez"
+        variant="dark"
+      />
+      <Card
+        balance="2125"
+        expiry="09/26"
+        cardNumber="5678 1234 9012"
+        name="Zariah Gomez"
+        variant="white"
+      />
+      <Card
+        balance="9025"
+        expiry="07/28"
+        cardNumber="5678 9012 1234"
+        name="Emmalynn Villar"
+        variant="dark"
+      />
+      <Card
+        balance="2125"
+        expiry="09/26"
+        cardNumber="5678 1234 9012"
+        name="Liliana Johnston"
+        variant="white"
+      />
     </div>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,28 +1,72 @@
-function Card() {
+interface CardProps {
+  balance: string
+  name: string
+  expiry: string
+  cardNumber: string
+  variant?: string
+}
+
+interface LabelValueProps {
+  label: string
+  value: React.ReactNode
+}
+
+function LabelValue(props: LabelValueProps) {
   return (
-    <div id="card-box">
-      <header id="first-line">
-        <div id="insiders">
-          <p className="headings">Balance</p>
-          <p className="content">$5,756</p>
+    <div>
+      <label className="text-xs leading-none whitespace-nowrap opacity-50">{props.label}</label>
+      <h4 className="font-bold text-lg leading-none whitespace-nowrap">{props.value}</h4>
+    </div>
+  )
+}
+
+function Card(props: CardProps) {
+  const { balance, cardNumber, expiry, name, variant = 'dark' } = props
+  const isDark = variant === 'dark'
+  const darkStyle = {
+    container: 'bg-gradient-to-r from-gray-600 to-black text-white',
+    footer: 'bg-gradient-to-r from-white/10 to-white/0'
+  }
+  return (
+    <div
+      id="card-box"
+      className={`${isDark ? darkStyle.container : 'bg-white text-black'} rounded-3xl w-80 min-w-80 max-w-96 m-[14px] font-sans flex flex-col justify-between overflow-hidden box-border`}
+    >
+      <header id="first-line" className="flex flex-row justify-between px-[30px] py-[22px]">
+        <div id="insiders" className="w-[63px] h-[38px]">
+          <LabelValue label="Balance" value={`$${balance}`} />
         </div>
-        <img id="chip" src="chip.png" alt="chip" />
+        <img
+          id="chip"
+          src={isDark ? 'chip.png' : 'card-chip-dark.svg'}
+          alt="chip"
+          className="w-[34.89px] h-[34.89px] flex items-center justify-center"
+        />
       </header>
-      <section id="second-line">
+      <section id="second-line" className="flex gap-10 px-[30px] py-[6px]">
         <span id="user-detail">
-          <p className="headings">CARD HOLDER</p>
-          <h4 className="content">Eddy Cusuma</h4>
+          <LabelValue label="CARD HOLDER" value={name} />
         </span>
         <span id="card-expiry">
-          <p className="headings">VALID THRU</p>
-          <h4 className="content">12/22</h4>
+          <LabelValue label="VALID THRU" value={expiry} />
         </span>
       </section>
-      <footer id="third-line">
-        <span id="card-number">3778 **** **** 1234</span>
-        <span id="card-icon">
-          <div id="cir-1"></div>
-          <div id="cir-2"></div>
+      <footer
+        id="third-line"
+        className={`${isDark ? darkStyle.footer : 'bg-white text-black'} flex justify-between items-center px-[30px] py-[20px] mt-3.5 text-lg`}
+      >
+        <span id="card-number" className="font-bold text-lg tracking-widest">
+          {cardNumber}
+        </span>
+        <span id="card-icon" className="flex">
+          <div
+            id="cir-1"
+            className={`${isDark ? 'bg-[rgba(240,236,236,0.5)]' : 'bg-[rgba(0,0,0,0.5)]'} w-[30px] h-[30px] rounded-full`}
+          ></div>
+          <div
+            id="cir-2"
+            className={`${isDark ? 'bg-[rgba(240,236,236,0.5)]' : 'bg-[rgba(0,0,0,0.5)]'} w-[30px] h-[30px] rounded-full -ml-[15px]`}
+          ></div>
         </span>
       </footer>
     </div>
